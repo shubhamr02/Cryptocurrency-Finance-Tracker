967cab0a23400bd9fadd45d9c8194b3ee0d94973
shubhamr02 <33284251+shubhamr02@users.noreply.github.com>
2021-03-03 19:22:32 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Coin from './Coin'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
